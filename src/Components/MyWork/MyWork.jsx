711b34394974dc2b1d54d60f4ff79a5f2c25c339
shreyareddy <32663313+shreyareddy6@users.noreply.github.com>
2024-09-20 16:04:27 -0400

    add more button

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,28 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MyWork.css";
 import theme_pattern from "../../assets/theme_pattern.svg";
 import mywork_data from "../../assets/mywork_data";
 import arrow_icon from "../../assets/arrow_icon.svg";
 
 const MyWork = () => {
+  // State to track how many works are visible
+  const [visibleWorks, setVisibleWorks] = useState(3); // Show 3 projects initially
+
+  // Function to load more projects
+  const loadMoreWorks = () => {
+    setVisibleWorks((prevCount) => prevCount + 3); // Load 3 more projects
+  };
+
+  // Function to show fewer projects (collapse)
+  const showLessWorks = () => {
+    setVisibleWorks(3); // Reset to showing only 3 projects
+  };
+
   return (
     <div className="mywork">
       <div className="mywork-title">
-        <h1>My latest work</h1>
-        <img src={theme_pattern} alt="" />
+        <h1>My work</h1>
+        <img src={theme_pattern} alt="Pattern" />
       </div>
+
+      {/* Display only the visible projects */}
       <div className="mywork-container">
-        {mywork_data.map((work) => {
+        {mywork_data.slice(0, visibleWorks).map((work) => {
           return (
-            <a key={work.w_no} href={work.w_url} target="_blank">
-              <img src={work.w_img} alt="" />
+            <a
+              key={work.w_no}
+              href={work.w_url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={work.w_img} alt={work.w_name} />
             </a>
           );
         })}
       </div>
-      <div className="mywork-showmore">
-        <p>Show More</p>
-        <img src={arrow_icon} alt="" />
+
+      {/* Show/Hide Buttons Section */}
+      <div className="mywork-buttons">
+        {visibleWorks < mywork_data.length && (
+          <div
+            className="mywork-button mywork-showmore"
+            onClick={loadMoreWorks}
+          >
+            <p>Show More</p>
+            <img src={arrow_icon} alt="Show more arrow" />
+          </div>
+        )}
+
+        {visibleWorks > 3 && (
+          <div
+            className="mywork-button mywork-showless"
+            onClick={showLessWorks}
+          >
+            <p>Show Less</p>
+            <img src={arrow_icon} alt="Show less arrow" />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,7 +1,8 @@
 import project1_img from "../assets/gameday_grub.png";
-import project2_img from "../assets/blog.png";
-import project3_img from "../assets/portfolio.png";
-import project4_img from "../assets/flipit.png";
+import project2_img from "../assets/ecommerce.png";
+import project3_img from "../assets/flipit.png";
+import project4_img from "../assets/portfolio.png";
+import project5_img from "../assets/blog.png";
 
 const mywork_data = [
   {
@@ -12,21 +13,27 @@ const mywork_data = [
   },
   {
     w_no: 2,
-    w_name: "Blog Book",
+    w_name: "Ecommerce Platform",
     w_img: project2_img,
-    w_url: "https://shreyareddy6.github.io/my-first-blog/",
+    w_url: "https://mern-ecommerce-platform-nbf8.onrender.com/",
   },
   {
     w_no: 3,
-    w_name: "portfolio",
+    w_name: "flipit",
     w_img: project3_img,
-    w_url: "https://shreyareddy6.github.io/Portfolio/",
+    w_url: "https://flipit-57ng.onrender.com/",
   },
   {
     w_no: 4,
-    w_name: "flipit",
+    w_name: "portfolio",
     w_img: project4_img,
-    w_url: "https://flipit-57ng.onrender.com/welcome",
+    w_url: "https://shreyareddy6.github.io/Portfolio/",
+  },
+  {
+    w_no: 5,
+    w_name: "Blog Book",
+    w_img: project5_img,
+    w_url: "https://shreyareddy6.github.io/my-first-blog/",
   },
 ];
 
